6d16b80e6403c2cc2f2577f00ae4f7ffd54f528e
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2023-10-18 12:45:24 +0400

    commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+
+    int a = 21;
+    int b = 3;
+
+    printf("Before: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After: a = %d, b = %d\n", a, b);
+}
